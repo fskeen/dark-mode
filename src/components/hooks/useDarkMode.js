2388d1ead4ae90f2eb859fb401b661fdc7ac6f11
Faye Skeen <fskeen@gmail.com>
2019-07-25 16:03:54 -0700

    It works. MVP met.

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage"
 
-function useDarkMode () {
+export function useDarkMode () {
+    const [darkMode, setDarkMode] = useLocalStorage("darkMode", false)
 
+    useEffect(() => {
+        if (darkMode===false) {
+            document.querySelector("body").classList.add("dark-mode")
+        } else {
+            document.querySelector("body").classList.remove("dark-mode")
+        }
+    }, [ darkMode ])
+
+    return [darkMode, setDarkMode];
 }
-
-export default useDarkMode;
--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function useLocalStorage (key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
@@ -9,7 +9,7 @@ function useLocalStorage (key, initialValue) {
         setStoredValue(value);
         window.localStorage.setItem(key, JSON.stringify(value));
     };
-    return [storedValue];
+    return [storedValue, setValue];
 }
 
 export default useLocalStorage;
